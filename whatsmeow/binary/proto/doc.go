d53aa60e649c3cb5b35d9e00855a2db3ce73b59b
kevzy <njirokevz@gmail.com>
2024-06-18 22:24:28 +0300

    pixel

---
+++ whatsmeow/binary/proto/doc.go
@@ -0,0 +1,6 @@
+// Package proto contains type aliases for backwards compatibility.
+//
+// Deprecated: New code should reference the protobuf types in the go.mau.fi/whatsmeow/proto/wa* packages directly.
+package proto
+
+//go:generate ./generatelegacy.sh
